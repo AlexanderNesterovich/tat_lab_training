293e944f1cd3e670ad942aacaa13b809c725ebf5
Aliaksandr Nestsiarovich <Aliaksandr_Nestsiarovich@epam.com>
2016-04-20 17:27:48 +0300

    fixes

--- TestConversion/src/by/training/Main.java
+++ TestConversion/src/by/training/Main.java
@@ -10,26 +10,27 @@ public class Main {
         long max = 9223372036854315137L;
         double same = max;
 
-        System.out.println("long: " + max);
+        System.out.println("actual long:   " + max);
         System.out.println("actual double: " + new BigDecimal(same).toString());
         System.out.println("sysout double: " + same);
 
         System.out.println("difference: " + (max - (long)same));
 
         System.out.println();
+
+
         //precision float to double
+        float f = 1.123456789123456789123456789123456781234567890f;
+        double sameD = f;
 
-        float f = 12.1234567890123456789345674567f;
-        double d = f;
+        System.out.println("sysout float:  " + f); //tricky printing
+        System.out.println("sysout double: " + sameD); //tricky printing
 
-        System.out.println("sysout float: " + f); //System.out.println tricks for "cool" printing
-        System.out.println("sysout double: " + d); //System.out.println tricks for "cool" printing
+        System.out.println("actual float:  " + new BigDecimal(f).toString()); //raw numbers
+        System.out.println("actual double: " + new BigDecimal(sameD).toString()); //raw numbers
 
-        System.out.println("actual float: " + new BigDecimal(f).toString()); //actual numbers
-        System.out.println("actual double: " + new BigDecimal(d).toString()); //actual numbers
-
-        System.out.println("difference1: " + new BigDecimal(d - f).toString());
-        System.out.println("difference2: " + (d - f));
+        System.out.println("difference1: " + new BigDecimal(sameD - f).toString());
+        System.out.println("difference2: " + (sameD - f));
 
     }
 }
